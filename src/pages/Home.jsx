ef5f2056340ba3a5bc91a2994492fa40e9a0e491
Mohamed El Morjani <elmorjanimohamed9@gmail.com>
2024-10-15 22:17:16 +0100

    feat: create HeroSection, AboutSection, ServiceCards, DeliveryTracking, and Contact components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+// src/pages/Home.jsx
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import ServiceCards from "../components/ServiceCards";
+import DeliveryTracking from "../components/DeliveryTracking";
+import Contact from "../components/Contact";
+
+const Home = () => {
+  return (
+    <div className="bg-slate-50 dark:bg-slate-900">
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* About Section */}
+      <AboutSection />
+
+      {/* Services Section */}
+      <ServiceCards />
+
+      {/* Delivery Tracking Section */}
+      <DeliveryTracking />
+
+      {/* Contact Section */}
+      <Contact />
+    </div>
+  );
+};
+
+export default Home;
