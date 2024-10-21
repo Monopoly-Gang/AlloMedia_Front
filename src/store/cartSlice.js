68c26e11826bb9e0a512f06df039b2130f97220c
aymanebs <aymane.bisdaoune@gmail.com>
2024-10-21 15:39:46 +0100

    AF-34 feat: Initialize cart slice with add, remove, and clear functionality

---
+++ src/store/cartSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    items : []
+};
+
+const cartSlice = createSlice({
+    name:"cart",
+    initialState,
+
+    reducers : {
+        addToCart:(state, action) => {
+            const product = action.payload;
+            // Check if item already in cart 
+            const existingItem = state.items.find(item=>item.id == product.id);
+            // Update quantity if item exists
+            if(existingItem) existingItem.quantity = product.quantity;
+            // Push a new item if not
+            else state.items.push({id:product.id,name:product.name,quantity:product.quantity || 1,price:product.price,})
+        },
+        removeFromCart:(state,action)=>{
+            const productId = action.payload;
+            state.items = state.items.filter((item) => productId !== item.id)
+        },
+        clearCart:(state,action) => {
+            state.items =[];
+        }
+    }
+
+})
+
+export const {addToCart, removeFromCart} = cartSlice.actions;
+export default cartSlice.reducers;
