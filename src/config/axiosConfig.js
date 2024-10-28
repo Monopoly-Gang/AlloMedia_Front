edcbb20f817778607a5aa5d78ba06e9ffc5749b3
aymanebs <aymane.bisdaoune@gmail.com>
2024-10-28 10:13:28 +0100

    modified env file

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL : `${import.meta.env.VITE_API_HOST}/api/`,
+    baseURL : `${import.meta.env.VITE_API_HOST}`,
     headers : {
         "Content-Type":"application/json",
     },
