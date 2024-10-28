f22b954928aecb2ae560f25e4c627b3915d1b4f7
youness <younesserbai@gmail.com>
2024-10-28 19:19:47 +0100

    feat(RestaurantCard): add delete functionality and pass state setter
    - Implemented `handelDelete` function to delete restaurants and update state
    - Passed `setRestaurants` to `RestaurantCard` for state management
    - Added success and error toasts for delete feedback

--- src/components/RestaurantDetails/RestaurantCard.jsx
+++ src/components/RestaurantDetails/RestaurantCard.jsx
@@ -1,10 +1,25 @@
 import { MapPin, Mail, Phone, Edit, Trash2 } from "lucide-react";
 import PropTypes from 'prop-types';
 import { useTranslation } from "react-i18next";
+import axiosInstance from "../../config/axios";
+import { toast } from "sonner";
 const Image_URL = import.meta.env.VITE_RESTO_IMG_SERVER;
-const RestaurantCard = ({ restaurant, onViewDetails }) => {
+
+
+const RestaurantCard = ({ restaurant, onViewDetails ,setRestaurants}) => {
   const { t } = useTranslation();
 
+
+  const handelDelete = async (id) => {
+    try {
+      console.log(id);
+      await axiosInstance.delete(`/restaurants/${id}`);
+      setRestaurants((prevRestaurants) => prevRestaurants.filter((restaurant) => restaurant._id !== id));
+      toast.success(t("Restaurant deleted successfully"));
+    } catch (error) {
+      toast.error(t(error.message));
+    }
+  };
   return (
     <div className="bg-white dark:bg-slate-900 rounded-lg shadow-sm border border-slate-200 dark:border-slate-700 overflow-hidden">
       <div className="p-4 flex flex-col items-center">
@@ -49,16 +64,17 @@ const RestaurantCard = ({ restaurant, onViewDetails }) => {
         <div className="flex justify-between items-center w-full mt-8">
           <button
             className="w-1/2 bg-primary text-white py-2 rounded-lg hover:bg-primary-dark transition duration-300"
-            onClick={() => onViewDetails(restaurant.id)}
+            onClick={() => onViewDetails(restaurant._id)}
           >
             {t("View Details")}
           </button>
+          
           <div className="flex flex-row items-center justify-center">
             <button className="flex items-center text-green-500 mr-2 rounded-full bg-slate-200 dark:bg-slate-800 p-2">
               <Edit size={16} />
             </button>
             <button className="flex items-center text-red-500 rounded-full bg-slate-200 dark:bg-slate-800 p-2">
-              <Trash2 size={16} />
+              <Trash2 size={16}  onClick={()=>handelDelete(restaurant._id)} />
             </button>
           </div>
         </div>
@@ -70,6 +86,7 @@ const RestaurantCard = ({ restaurant, onViewDetails }) => {
 RestaurantCard.propTypes = {
   restaurant: PropTypes.object.isRequired,
   onViewDetails: PropTypes.func.isRequired,
+  setRestaurants: PropTypes.func.isRequired,
 };
 
 export default RestaurantCard;
